82bc8cabca39374f7d529957010a170f9060db7b
JakeClark33 <84428608+JakeClark33@users.noreply.github.com>
2021-06-27 19:17:47 -0500

    help

---
+++ README.md
@@ -0,0 +1,4 @@
+Nothing needs to be loaded to run this file.
+
+This quiz is built using JavaScript, CSS and HTML.
+
---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+
+    {
+        order: 1,
+        question: 'Commonly used data types do not include?',
+        answer: 3,
+        choices: [
+            "Numbers",
+            "Booleon",
+            "Alerts",
+            "Strings",
+        ]
+        },
+    {
+        order: 2,
+        question: 'The condiution in an if/else statement ins enclosed in?',
+        answer: 1,
+        choices: [
+            "Parentheses",
+            "Curly Brackets",
+            "Square Brackets",
+            "Quotations",
+        ]
+    },
+    {   order: 3,
+        question: 'A very useful tool during development and debugging for printing content to the debugger is?',
+        answer: 2,
+        choices:[ 
+            "Console.log",
+            "GitBash Terminal",
+            "for loop",
+            "JavaScript",
+        ]
+    },
+    {   
+        order: 4,
+        question:'Arrays in JavaScript can be used to store?',
+        answer: 4,
+        choices:[ 
+            "Booleon",
+            "Other Arrays",
+            "Numbers and Strings",
+            "All of the Above",
+         ]
+        },
+    {
+        order: 5,
+        question:'String values must be enclosed with _______ when being assigned to a variable?',
+        answer: 2,
+        choices:[ 
+            "Curly Brackets",
+            "Square Brackets",
+            "Parentheses",
+            "Commas",
+        ]
+        },   
+    ];
