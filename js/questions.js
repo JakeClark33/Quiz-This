d32f3979a816fcb1af70e28a000be30fe27758b9
JakeClark33 <84428608+JakeClark33@users.noreply.github.com>
2021-08-02 21:08:23 -0500

    questions

--- js/questions.js
+++ js/questions.js
@@ -1,57 +1,178 @@
-let questions = [
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+Create an event listener toi start the timer interval.a
 
-    {
-        order: 1,
-        question: 'Commonly used data types do not include?',
-        answer: 3,
-        choices: [
-            "Numbers",
-            "Booleon",
-            "Alerts",
-            "Strings",
-        ]
+ THEN a timer starts and I am presented with a question
+
+
+// WHEN I answer a question
+
+// Create an array of all of the questions
+// All questions will need a title multiple choices and an answer.
+ Creeate a function that will ask one question at a time.
+
+
+
+ function generateQuiz (questions, quizContainer, resultsContainer, SubmitButton) {
+
+
+    function displayQuestions (questions, QuizContainer){
+
+
+    }
+
+    function displayResults(questions, quizContainer, resultsContainer){
+
+
+    }
+    
+    
+    displayQuestions(questions, QuizContainer);
+
+
+    submitButton.onclick = function(){
+
+            displayResults(questions, quizContainer, resultsContainer);
+    }
+
+ }
+
+ var quizQuestions = [
+     {
+        question: "String Values must be enclosed with __________ when being assigned to variables?",
+
+        answers: {
+
+            a: 'Curly Brackets',
+
+            b: 'Parentheses',
+
+            c: 'Backticks',
+
+            d: 'commas'
+
         },
-    {
-        order: 2,
-        question: 'The condiution in an if/else statement ins enclosed in?',
-        answer: 1,
-        choices: [
-            "Parentheses",
-            "Curly Brackets",
-            "Square Brackets",
-            "Quotations",
-        ]
-    },
-    {   order: 3,
-        question: 'A very useful tool during development and debugging for printing content to the debugger is?',
-        answer: 2,
-        choices:[ 
-            "Console.log",
-            "GitBash Terminal",
-            "for loop",
-            "JavaScript",
-        ]
-    },
-    {   
-        order: 4,
-        question:'Arrays in JavaScript can be used to store?',
-        answer: 4,
-        choices:[ 
-            "Booleon",
-            "Other Arrays",
-            "Numbers and Strings",
-            "All of the Above",
-         ]
+
+            correctAnswer: 'a'
+
+     },
+
+            
+
+     {
+
+        question:'Commonly used data types do not include _______.',
+
+        answers: {
+
+            a: 'Booleans',
+
+            b: 'Alerts',
+
+            c: 'Strings',
+
+            d: 'Numbers',
+
         },
-    {
-        order: 5,
-        question:'String values must be enclosed with _______ when being assigned to a variable?',
-        answer: 2,
-        choices:[ 
-            "Curly Brackets",
-            "Square Brackets",
-            "Parentheses",
-            "Commas",
-        ]
-        },   
-    ];
+
+            correctAnswer: 'b'
+
+     },
+
+
+     
+     {
+
+        question: 'The condition in an if/else statement is enclosed with ____________.',
+
+        answers: {
+
+            a: 'Curly Brackets',
+
+            b: 'Backticks',
+
+            c: 'Commas',
+
+            d: 'Parentheses',
+
+        },
+
+            correctAnswer: 'd',
+
+     },
+
+
+     {
+     
+        question: 'Arrays in JavaScript can be used to store ___________.',
+
+        answers: {
+            a: 'Booleans',
+
+            b: 'Numbers and Arrays',
+
+            c: 'Strings',
+
+            d: 'All of the above',
+       
+        },
+
+            correctAnswer: 'd'
+
+     },
+
+
+     
+     {
+
+        question: 'A useful tool for development and debugging for printing to the debugger is _____________.',
+
+        answers: {
+
+            a: 'For Loops',
+
+            b: 'JavaScript',
+
+            c: 'Terminal Bash',
+
+            d: 'All of the above',
+
+        },
+
+        correctAnswer: 'a'
+
+     },
+
+
+
+
+
+ create a time interval
+ 
+ 
+ 
+ create a certain amoiunt of time to give the user. Counting down from 60-0. 
+ 
+ 
+ 
+ decrease time by 5 seconds when questions are answered incorrectly.
+
+
+// THEN I am presented with another question
+
+// WHEN I answer a question incorrectly
+
+// THEN time is subtracted from the clock
+
+// WHEN all questions are answered or the timer reaches 0
+
+
+// THEN the game is over
+
+
+// WHEN the game is over
+use local storage to keep the score.
+// THEN I can save my initials and score
+pull from local storage to see top scores. 
+given ability to enter user initials for high score.a
+high scores created on a separate page. 
